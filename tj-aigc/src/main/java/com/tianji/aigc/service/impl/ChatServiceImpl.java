ca58237bcc4485ef130ff800cfa15f3746a39152
Aries <3358248521@qq.com>
2026-06-30 19:03:57 +0800

    1

--- tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
+++ tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
@@ -5,10 +5,12 @@ import com.tianji.aigc.config.SystemPromptConfig;
 import com.tianji.aigc.enums.ChatEventTypeEnum;
 import com.tianji.aigc.service.ChatService;
 import com.tianji.aigc.vo.ChatEventVO;
+import com.tianji.common.utils.CollUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -26,11 +28,14 @@ public class ChatServiceImpl implements ChatService {
     // 存储大模型的生成状态，这里采用ConcurrentHashMap是确保线程安全
     // 目前的版本暂时用Map实现，如果考虑分布式环境的话，可以考虑用redis来实现
     private static final Map<String, Boolean> GENERATE_STATUS = new ConcurrentHashMap<>();
+    private final ChatMemory chatMemory;
 
     @Override
     public Flux<ChatEventVO> chat(String question, String sessionId) {
         // 获取对话id
         var conversationId = ChatService.getConversationId(sessionId);
+        // 大模型输出内容的缓存器，用于在输出中断后的数据存储
+        var outputBuilder = new StringBuilder();
 
         return this.chatClient.prompt()
                 .system(promptSystem -> promptSystem
@@ -48,11 +53,17 @@ public class ChatServiceImpl implements ChatService {
                     GENERATE_STATUS.remove(sessionId);
                 })
                 .doOnError(throwable -> GENERATE_STATUS.remove(sessionId)) // 错误时清除标记
+                .doOnCancel(() -> {
+                    // 当输出被取消时，保存输出的内容到历史记录中
+                    this.saveStopHistoryRecord(conversationId, outputBuilder.toString());
+                })
                 // 输出过程中，判断是否正在输出，如果正在输出，则继续输出，否则结束输出
                 .takeWhile(s -> Optional.ofNullable(GENERATE_STATUS.get(sessionId)).orElse(false))
                 .map(chatResponse -> {
                     // 获取大模型的输出的内容
                     String text = chatResponse.getResult().getOutput().getText();
+                    // 追加到输出内容中
+                    outputBuilder.append(text);
                     // 封装响应对象
                     return ChatEventVO.builder()
                             .eventData(text)
@@ -62,6 +73,16 @@ public class ChatServiceImpl implements ChatService {
                 .concatWith(Flux.just(ChatEventVO.builder()  // 标记输出结束
                         .eventType(ChatEventTypeEnum.STOP.getValue())
                         .build()));
+    }
+
+    /**
+     * 保存停止输出的记录
+     *
+     * @param conversationId 会话id
+     * @param content        大模型输出的内容
+     */
+    private void saveStopHistoryRecord(String conversationId, String content) {
+        this.chatMemory.add(conversationId, new AssistantMessage(content));
     }
 
     @Override
