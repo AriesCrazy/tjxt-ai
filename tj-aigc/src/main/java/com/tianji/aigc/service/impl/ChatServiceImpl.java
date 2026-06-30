394c4337f235090e02a761846648093cbe415b99
Aries <3358248521@qq.com>
2026-06-30 14:52:07 +0800

    1

--- tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
+++ tj-aigc/src/main/java/com/tianji/aigc/service/impl/ChatServiceImpl.java
@@ -5,7 +5,6 @@ import com.tianji.aigc.config.SystemPromptConfig;
 import com.tianji.aigc.enums.ChatEventTypeEnum;
 import com.tianji.aigc.service.ChatService;
 import com.tianji.aigc.vo.ChatEventVO;
-import com.tianji.common.utils.DateUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
@@ -13,6 +12,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Service
@@ -20,19 +21,27 @@ import java.util.Map;
 public class ChatServiceImpl implements ChatService {
 
     private final ChatClient chatClient;
-
     private final SystemPromptConfig systemPromptConfig;
-
+    // 存储大模型的生成状态，这里采用ConcurrentHashMap是确保线程安全
+    // 目前的版本暂时用Map实现，如果考虑分布式环境的话，可以考虑用redis来实现
+    private static final Map<String, Boolean> GENERATE_STATUS = new ConcurrentHashMap<>();
 
     @Override
     public Flux<ChatEventVO> chat(String question, String sessionId) {
         return this.chatClient.prompt()
                 .system(promptSystem -> promptSystem
-                        .text(this.systemPromptConfig.getChatSystemMessage().get())
-                        .params(Map.of("now", DateUtil.now())))
+                        .text(this.systemPromptConfig.getChatSystemMessage().get()) // 设置系统提示语
+                        .param("now", DateUtil.now()) // 设置当前时间的参数
+                )
                 .user(question)
                 .stream()
                 .chatResponse()
+                .doFirst(() -> GENERATE_STATUS.put(sessionId, true)) // 第一次输出内容时执行
+                .doOnError(throwable -> GENERATE_STATUS.remove(sessionId)) // 出现异常时，删除标识
+                .doOnComplete(() -> GENERATE_STATUS.remove(sessionId)) // 完成时执行，删除标识
+                .takeWhile(response -> { // 通过返回值来控制Flux流是否继续，true：继续，false：终止
+                    return GENERATE_STATUS.getOrDefault(sessionId, false);
+                })
                 .map(chatResponse -> {
                     // 获取大模型的输出的内容
                     String text = chatResponse.getResult().getOutput().getText();
@@ -45,5 +54,11 @@ public class ChatServiceImpl implements ChatService {
                 .concatWith(Flux.just(ChatEventVO.builder()  // 标记输出结束
                         .eventType(ChatEventTypeEnum.STOP.getValue())
                         .build()));
+    }
+
+    @Override
+    public void stop(String sessionId) {
+        // 移除标记
+        GENERATE_STATUS.remove(sessionId);
     }
 }
